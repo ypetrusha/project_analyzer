baeabbef148e902dd1601ba11809c0a6022012d7
ypetrusha <ypetrusha@protonmail.com>
2023-06-21 02:17:15 +0400

    config.py

--- main.py
+++ main.py
@@ -4,71 +4,7 @@ import tkinter as tk
 from tkinter import filedialog, messagebox
 from tkinter import ttk  # import ttk for the Combobox widget
 
-prompts = {
-    "initial_prompt": "You will be provided with project files one by one including relative file path and EOF mark."
-                      "In response reply exactly 'EOF received' or 'EOF not found'.",
-
-    "analyzer_prompt": "You are software developer profound in python coding. "
-                       "You will be asked to refactor the code, add features and test coverage."
-}
-
-functions = [
-    {
-        "name": "get_updated_files",
-        "description": "Project files created/updated/deleted according to user request",
-        "parameters": {
-            "type": "object",
-            "properties": {
-                "files": {
-                    "type": "array",
-                    "items": {
-                        "type": "object",
-                        "properties": {
-                            "path": {
-                                "type": "string",
-                                "description": "Relative path of the file"
-                            },
-                            "action": {
-                                "type": "string",
-                                "enum": ["create", "update", "delete"],
-                                "description": "action performed on file"
-                            },
-                            "content": {
-                                "type": "string",
-                                "description": "Full content of the file"
-                            },
-                        },
-                    },
-                    "description": "Array of files affected as result of user request",
-                },
-                "comment": {
-                    "type": "string",
-                    "description": "Assistant comment"
-                }
-            },
-            "required": ["files"],
-        },
-    },
-
-    {
-        "name": "get_git_patch",
-        "description": "Git patch according to user request",
-        "parameters": {
-            "type": "object",
-            "properties": {
-                "patch": {
-                    "type": "string",
-                    "description": "Git patch",
-                },
-                "comment": {
-                    "type": "string",
-                    "description": "Assistant comment"
-                }
-            },
-            "required": ["patch", "comment"],
-        },
-    }
-]
+from config import prompts, functions
 
 
 class ProjectAnalyzer:
