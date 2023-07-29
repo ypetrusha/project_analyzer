e6f9fa71f72e1543be3dd7a371c0ec1daef1aa66
ypetrusha <ypetrusha@protonmail.com>
2023-07-29 15:20:28 +0400

    introduce .env

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 import tkinter as tk
+
+import openai
+
 from project_analyzer import ProjectAnalyzer
 from ui import ProjectAnalyzerUI
+import dotenv
+import os
 
 
 def main():
+    dotenv.load_dotenv()
+    openai.api_key = os.getenv("OPENAI_API_KEY")
+    api_base = os.getenv("OPENAI_API_BASE")
+    if api_base:
+        openai.api_base = api_base
     root = tk.Tk()
     analyzer = ProjectAnalyzer()
     ui = ProjectAnalyzerUI(root, analyzer)
